9ebb0e95d30786749a061f697c8c57522ffe1585
Nicolás Antonelli <niconelli2@gmail.com>
2020-01-30 05:12:11 -0300

    Finished Vue's class 01 to 03, Added Bootstrap

--- Vue Practice/main.js
+++ Vue Practice/main.js
@@ -2,18 +2,36 @@ const app = new Vue({
     el: '#app',
     data: {
         titulo: "Hello World with Vue",
+        fondo: "bg-danger",
+        color: true,
         frutas: [
             {nombre: 'Manzana', cantidad: 10},
             {nombre: 'Naranja', cantidad: 0},
             {nombre: 'Banana', cantidad: 11}
         ],
-        nuevaFruta: ""
+        nuevaFrutaNombre: "",
+        nuevaFrutaCantidad: 0,
+        total: 0,
     },
     methods: {
         // agregarFruta: function(){
-        agregarFruta () {
-            this.frutas.push({nombre: this.nuevaFruta, cantidad: 0});
+        agregarFruta() {
+            this.frutas.push({
+                nombre: this.nuevaFrutaNombre,
+                cantidad: this.nuevaFrutaCantidad
+            });
             console.log("Elemento Agregado");
+            this.nuevaFrutaNombre = "";
+            this.nuevaFrutaCantidad = 0;
+        }
+    },
+    computed: {
+        sumarFrutas() {
+            this.total = 0;
+            for(fruta of this.frutas) {
+                this.total += fruta.cantidad;
+            }
+            return this.total;
         }
     }
 });
